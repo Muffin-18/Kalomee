e5e306bf5bb10e3db1ad7d9c6f6f4420464a63ea
Muffin-18 <ashmitjena@gmail.com>
2026-03-27 17:38:32 +0530

    deleted loading.py and modified model.py

--- backend/model.py
+++ backend/model.py
@@ -7,6 +7,41 @@ from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications.resnet50 import preprocess_input
 import numpy as np
 
+# Load datasets
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    config.DATA_DIR,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=config.IMG_SIZE,
+    batch_size=config.BATCH_SIZE
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    config.DATA_DIR,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=config.IMG_SIZE,
+    batch_size=config.BATCH_SIZE
+)
+
+class_names = train_ds.class_names
+num_classes = len(class_names)
+print(f"Detected {num_classes} classes.")
+
+#Perfomance optimization
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.shuffle(20000).prefetch(AUTOTUNE)
+val_ds = val_ds.prefetch(AUTOTUNE)
+
+# DATA AUGMENTATION
+data_augmentation = keras.Sequential([
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1)
+])
+
 base_model = ResNet50(
     weights="imagenet",
     include_top=False,
@@ -14,7 +49,7 @@ base_model = ResNet50(
 )
 base_model.trainable = False
 inputs = keras.Input(shape=config.IMG_SIZE + (3,))
-x = loading.data_augmentation(inputs)
+x = data_augmentation(inputs)
 x = preprocess_input(x)
 x = base_model(x, training=False)
 x = layers.GlobalAveragePooling2D()(x)
@@ -31,7 +66,7 @@ model.compile(
 
 print("\nTraining model...")
 history = model.fit(
-    loading.train_ds,
+    train_ds,
     validation_data=loading.val_ds,
     epochs=config.EPOCHS
 )
@@ -49,7 +84,7 @@ model.compile(
 )
 
 history_ft = model.fit(
-    loading.train_ds,
+    train_ds,
     validation_data=loading.val_ds,
     epochs=10
 )
